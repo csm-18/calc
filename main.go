d1d11a9eb90976324d784fdd764f15b992dc80c0
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-08-04 21:56:46 +0530

    calc main loop

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+const VERSION = "v1.0.0"
+
 func main() {
-	println("Har Har Mahadev!")
+	fmt.Println("calc", VERSION)
+	for {
+		var input string
+		fmt.Println("enter expression('q' for quit):")
+		fmt.Scanln(&input)
+		if input == "q" || input == "Q" {
+			os.Exit(0)
+		}
+	}
 }
