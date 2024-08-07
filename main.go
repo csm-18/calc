09152ef0869e8c0ac59a539e9cf597897eb4d783
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-08-07 15:13:56 +0530

    handle empty input

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ func main() {
 	fmt.Println("calc", VERSION)
 	for {
 		var input string
-		fmt.Println("enter expression('q' for quit):")
+		var result string
+		fmt.Println("Enter expression('q' for quit):")
 		fmt.Scanln(&input)
-		if input == "q" || input == "Q" {
+		if len(input) == 0 {
+			continue
+		} else if input == "q" || input == "Q" {
 			os.Exit(0)
 		}
+
+		result = calc(input)
+		fmt.Println("Result:", result)
 	}
 }
