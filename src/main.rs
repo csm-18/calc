0a295818aa7a403feaaa86b8e4425e9dc1f7d0dd
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-04-12 21:16:14 +0530

    calculator main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{io::{stdin, stdout, Write}, process::exit};
+
+const VERSION:&str = "0.1.0";
 fn main() {
-    println!("Hello, world!");
+    println!("calc {VERSION}");
+    loop {
+        print!(">> ");
+        stdout().flush().unwrap();
+
+        //read input expression
+        let exp = input();
+        
+        if &exp == "\n" {
+            continue;
+        }
+        
+        dbg!(exp);
+    }
+}
+
+fn input()-> String{
+    let mut user_input = String::new();
+    match stdin().read_line(&mut user_input) {
+        Ok(_) => {
+            user_input
+        },
+        Err(_) => {
+            println!("internal error!");
+            exit(1);
+        }
+    }
 }
